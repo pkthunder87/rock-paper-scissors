1533577d9a35aba9a6c5953693cb126b4e17a464
pkthunder87 <pkthunder87@gmail.com>
2023-10-07 13:40:36 -0700

    Feat: Finish Header component styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import ScoreBox from './ScoreBox';
 
 function Header() {
   return (
-    <div className="flex h-[12.5dvh] w-[84dvw] items-center justify-between rounded border-2 border-header-outline p-2">
+    <div className="flex h-[12.5dvh] w-[84dvw] items-center justify-between rounded-lg border-4 border-header-outline p-2">
       <img
         className="h-[fit-content] w-[fit-content] scale-[.68]"
         src="logo-bonus.svg"
--- src/components/ScoreBox.jsx
+++ src/components/ScoreBox.jsx
@@ -1,8 +1,10 @@
 function ScoreBox() {
   return (
-    <div className=" mr-2 flex h-24 w-24 flex-col items-center rounded border-[3px] bg-white">
-      <h2 className="text-sm font-semibold text-score-text">SCORE</h2>
-      <h2 className=" text-[3rem] font-bold ">12</h2>
+    <div className=" mr-2 flex h-[5.8rem] w-[5.8rem] flex-col items-center rounded-lg border-[3px] bg-white">
+      <h2 className="-mb-3 pt-[.65rem] text-sm font-semibold tracking-widest text-score-text">
+        SCORE
+      </h2>
+      <h2 className=" text-[3rem] font-bold text-dark-text">12</h2>
     </div>
   );
 }
