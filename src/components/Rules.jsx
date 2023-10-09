745741892be77b515220ce6d26a9bd1c5814ddae
pkthunder87 <pkthunder87@gmail.com>
2023-10-09 12:12:48 -0700

    Feat: Finish Rules.jsx component

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -1,5 +1,11 @@
 function Rules() {
-  return <div className="">Rules</div>;
+  return (
+    <div className="mb-12 flex items-end justify-center">
+      <div className="flex h-[56px] w-[150px] items-center justify-center rounded-lg border-4 border-header-outline p-2 text-2xl">
+        <h1 className="tracking-widest text-[#eee]">RULES</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Rules;
