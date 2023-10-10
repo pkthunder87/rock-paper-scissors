47d0db229bf8c8d4ef9f3231c9879798d0bb9921
pkthunder87 <pkthunder87@gmail.com>
2023-10-10 13:24:04 -0700

    Feat: Add styling to Modal.jsx component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,17 @@
 function Modal({ onHandleToggleModal }) {
   return (
-    <div className="absolute inset-0 h-[100dvh] w-[100dvw] bg-white text-3xl text-blue-800">
-      MODAL
-      <button onClick={onHandleToggleModal}>CLOSE</button>
+    <div className="absolute inset-0 grid h-[100dvh] w-[100dvw]  grid-rows-[30%_40%_30%] items-center justify-center bg-white p-10 text-center">
+      <h2 className="text-[2.5rem] tracking-wide text-dark-text">RULES</h2>
+      <img
+        className=" scale-125"
+        src="./image-rules-bonus.svg"
+        alt="Rules Diagram"
+      />
+      <div className="mb-12 self-end">
+        <button onClick={onHandleToggleModal}>
+          <img className="h-10 w-10" src="./icon-close.svg" alt="X Mark" />
+        </button>
+      </div>
     </div>
   );
 }
