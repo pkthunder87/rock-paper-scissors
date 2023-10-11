e65455f9e8303ad266e58fd1fb21131e52449d7e
pkthunder87 <pkthunder87@gmail.com>
2023-10-11 15:38:40 -0700

    Feat: Add initial styling to ChosenGestures.jsx

--- src/components/ChosenGestures.jsx
+++ src/components/ChosenGestures.jsx
@@ -1,5 +1,37 @@
+import Circle from './Circle';
+import HandGesture from './HandGesture';
+
 function ChosenGestures() {
-  return <div className="bg-yellow-500">Chosen Gestures</div>;
+  return (
+    <div className="grid grid-cols-2">
+      <div className="-mt-4 ml-6">
+        <div>
+          <HandGesture
+            gesture="lizard"
+            gesture_color_from="from-lizard-from"
+            gesture_color_to="to-lizard-to"
+          />
+        </div>
+        <p className="ml-[1.35rem] mt-6 text-xl tracking-wide text-white">
+          You Picked
+        </p>
+      </div>
+
+      <div className=" -mt-4 mr-4 justify-self-end">
+        {/* <Circle /> */}
+        <div>
+          <HandGesture
+            gesture="spock"
+            gesture_color_from="from-spock-from"
+            gesture_color_to="to-spock-to"
+          />
+        </div>
+        <p className=" mt-6  text-xl tracking-wide text-white">
+          The House Picked
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ChosenGestures;
---
+++ src/components/Circle.jsx
@@ -0,0 +1,7 @@
+function Circle() {
+  return (
+    <div className="mb-8 mt-2 h-[7.5rem] w-[7.5rem] rounded-full bg-[hsl(222,44%,18%)]"></div>
+  );
+}
+
+export default Circle;
