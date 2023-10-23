c3783499fb4e66b9883ba3cc6b6f07441a585f46
pkthunder87 <pkthunder87@gmail.com>
2023-10-23 07:39:46 -0700

    Style: Finish desktop design for Rules.jsx

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -4,10 +4,10 @@ function Rules() {
   const { onToggleModal: onOpenModal } = useGameState();
 
   return (
-    <div className="mb-10 flex items-end justify-center sm:absolute sm:bottom-0 sm:right-8">
+    <div className="mb-10 flex items-end justify-center sm:absolute sm:-bottom-[.2rem] sm:right-[2.35rem]">
       <button
         onClick={onOpenModal}
-        className="flex h-[56px] w-[150px] items-center justify-center rounded-lg border-4 border-header-outline p-2 text-2xl tracking-widest text-[#eee]"
+        className="flex h-[56px] w-[150px] items-center justify-center rounded-lg border-4  border-header-outline p-2 text-2xl font-semibold tracking-widest text-[#eee] antialiased md:h-[42px] md:w-[130px] md:rounded-[10px] md:border-2 md:text-[1.35rem] md:tracking-[.15em]"
       >
         RULES
       </button>
