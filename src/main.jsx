c3a518633797183df1242e99fe346268669a58fb
pkthunder87 <pkthunder87@gmail.com>
2023-11-01 08:08:39 -0700

    Fix: Capitalize g to G of import from gameStateContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { GameStateProvider } from './context/gameStateContext';
+import { GameStateProvider } from './context/GameStateContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
